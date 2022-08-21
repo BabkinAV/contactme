0bf64b6b97ed4934040cc7cf8638e2a2d6e856cc
Andrey Babkin <babkin.andrey@gmail.com>
2022-08-21 10:31:15 +0300

    used reselector for contact array sort

--- src/store/slices/dataSlice.ts
+++ src/store/slices/dataSlice.ts
@@ -1,63 +1,78 @@
 import { createSlice, PayloadAction, createSelector } from '@reduxjs/toolkit';
 
 import type { RootState } from '../store';
-import {Contact} from '../../dataModel';
-import  {contactListArray} from '../../data';
+import { Contact } from '../../dataModel';
+import { contactListArray } from '../../data';
 
 interface contactState {
   contactList: Contact[];
-  filter: string
+  filter: string;
 }
 
 const initialState: contactState = {
   contactList: contactListArray,
-  filter: ''
+  filter: '',
 };
 
 export const dataSlice = createSlice({
   name: 'dataSlice',
   initialState,
   reducers: {
-    addContact: (state, action: PayloadAction<Contact>)=> {
-      state.contactList.push(action.payload)
+    addContact: (state, action: PayloadAction<Contact>) => {
+      state.contactList.push(action.payload);
     },
-    editContact: (state,action:PayloadAction<Contact> ) => {
-      const elementIndex = state.contactList.findIndex((arrEl => arrEl.id === action.payload.id));
+    editContact: (state, action: PayloadAction<Contact>) => {
+      const elementIndex = state.contactList.findIndex(
+        (arrEl) => arrEl.id === action.payload.id
+      );
       state.contactList[elementIndex] = action.payload;
     },
-    deleteContact: (state, action:PayloadAction<string>) => {
-      state.contactList.splice(state.contactList.findIndex((el) => el.id === action.payload), 1);
+    deleteContact: (state, action: PayloadAction<string>) => {
+      state.contactList.splice(
+        state.contactList.findIndex((el) => el.id === action.payload),
+        1
+      );
     },
-    setFilter: (state, action:PayloadAction<string>) => {
+    setFilter: (state, action: PayloadAction<string>) => {
       state.filter = action.payload;
-    }
-  }
+    },
+  },
 });
 
-const selectContacts = (state:RootState) => 
+const selectContacts = (state: RootState) => state.data.contactList;
 
-{
-  let sortedArr = [...state.data.contactList];
-  
-  sortedArr.sort((a, b) => a.lastName.localeCompare(b.lastName));
-  
-  return sortedArr};
-const selectFilter = (state:RootState) => state.data.filter;
+const selectFilter = (state: RootState) => state.data.filter;
 
-const selectContactId = (state:RootState, contactId: string) => contactId;
+const selectContactId = (state: RootState, contactId: string) => contactId;
 
-export const getFilteredContactsSelector = createSelector([selectContacts, selectFilter], (contactList, filter) => {
-  if (filter === '') {
-    return contactList;
-  } else {
-    const regex = new RegExp(filter, "i");
-    const newContactsArray = contactList.filter(el => regex.test(el.firstName) || regex.test(el.lastName) || regex.test(el.phoneNumber) || regex.test(el.email));
-    return newContactsArray;
+const getSortedContactsSelector = createSelector([selectContacts], (contactList) => {return contactList.slice(0).sort((a, b) => a.lastName.localeCompare(b.lastName));})
+
+export const getFilteredContactsSelector = createSelector(
+  [getSortedContactsSelector, selectFilter],
+  (sortedContactList, filter) => {
+    if (filter === '') {
+      return sortedContactList;
+    } else {
+      const regex = new RegExp(filter, 'i');
+      const newContactsArray = sortedContactList.filter(
+        (el) =>
+          regex.test(el.firstName) ||
+          regex.test(el.lastName) ||
+          regex.test(el.phoneNumber) ||
+          regex.test(el.email)
+      );
+      return newContactsArray;
+    }
   }
-})
+);
 
-export const getSingleContactSelector = createSelector([selectContacts, selectContactId], (contactList, contactId) => contactList.find(arrayEl => arrayEl.id === contactId ))
+export const getSingleContactSelector = createSelector(
+  [selectContacts, selectContactId],
+  (contactList, contactId) =>
+    contactList.find((arrayEl) => arrayEl.id === contactId)
+);
 
-export const {addContact, editContact, deleteContact, setFilter} = dataSlice.actions;
+export const { addContact, editContact, deleteContact, setFilter } =
+  dataSlice.actions;
 
 export default dataSlice.reducer;
